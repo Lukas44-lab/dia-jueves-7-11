480bcf10339111fa6fb5917d3417331d5f8b755a
lukas44 <lucasmadrid2@gmail.com>
2024-11-07 13:52:32 +0100

    change about.js

--- about.js
+++ about.js
@@ -1,1 +1,19 @@
 console.log('olaaswsa');
+
+// sentencias 
+
+//expreciones
+
+//variables y datos
+"use strict";
+console.log(22, typeof 22);
+console.log('pepe');
+console.log(true);
+x=23
+//declarar variable e asignar valor
+let user ='pepe';
+
+user='jose';
+surname='perez'
+
+console.log(`${user} ${surname}`);
