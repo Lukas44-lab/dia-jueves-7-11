fbae54d18128238fd76bb685621132636cfb5fee
lukas44 <lucasmadrid2@gmail.com>
2024-11-08 13:54:06 +0100

    change in index.html index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('holaaaaa');
+// function greeting(){
+//     const question='¿como te llamas?';
+//     const username =prompt(question);
+//     if(username !== null){
+//         const response='hola, '+username+' que tal estas';
+//     alert(response) 
+//     }
+    
+// }
+function greeting(){
+    const question='¿como te llamas?';
+    const username =prompt(question);
+    if(username === null || username==='')return;
+    const response='hola, '+username+' que tal estas';
+    alert(response) 
+    }
+const button=document.querySelector('form button');
+button.addEventListener('click', greeting);
+
